2b17d513f2c5c116977f478e3a14012310942cc9
Hatim Tagmi <125710473+theoutsider1@users.noreply.github.com>
2024-01-22 20:48:55 +0100

    Footer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,8 +2,9 @@ import './App.css';
 //import Nav from './components/nav';
 import Navbar from './components/Navbar/Navbar';
 import HeroSection from './components/HeroSection/HeroSection';
-import LatestOffer from './components/LatestOffers/Latestoffers';
+//import LatestOffer from './components/LatestOffers/Latestoffers';
 import Espace from './components/Navbar/BeforeNav/Espace';
+import Footer from './components/Footer/Footer';
 
 function App() {
   
@@ -13,6 +14,7 @@ function App() {
     <Espace/>
     <Navbar/>
     <HeroSection/>
+    <Footer/>
    
     
     
