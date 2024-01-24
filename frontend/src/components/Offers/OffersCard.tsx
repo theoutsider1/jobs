6cd1ff6c9286c752be77316a337e38c90f0bf5aa
Hatim Tagmi <125710473+theoutsider1@users.noreply.github.com>
2024-01-24 16:12:37 +0100

    offers Cards structure

--- frontend/src/components/Offers/OffersCard.tsx
+++ frontend/src/components/Offers/OffersCard.tsx
@@ -2,8 +2,20 @@ const OffersCard = () => {
 
 
     return (
-        <div>
-                
+        <div className="flex w-full py-14">
+
+
+            <div className="bg-white flex-none w-64 px-5">
+                <div className="bg-black">
+                    fhdfh
+                </div>
+            </div>
+            <div className="bg-blue-500 flex-auto">
+                <div className=" bg-white">
+        ha333
+                </div>
+            </div>
+
 
         </div>
     )
