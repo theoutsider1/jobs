f3ed973ed7ca66564223bd26ad4df373711d4b5e
Hatim Tagmi <125710473+theoutsider1@users.noreply.github.com>
2024-03-29 07:55:46 +0000

    seprate the AuthGuard
     by creating its own folder (guard)

--- backend/src/recruiter/recruiter.controller.ts
+++ backend/src/recruiter/recruiter.controller.ts
@@ -1,11 +1,12 @@
 import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { JwtGuard } from 'src/auth/guard';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Controller('recruteurs')
 export class RecruiterController {
     
-    @UseGuards(AuthGuard('jwt'))
+    @UseGuards(JwtGuard)
     @Get('profile')
 
      getProfil(@Req() req){
