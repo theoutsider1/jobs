ef2fefd6fd8b61ee917081cc6b73ed5894f76045
Hatim Tagmi <125710473+theoutsider1@users.noreply.github.com>
2024-04-24 14:57:32 +0100

    Fixed login form style

--- frontend/src/components/Recruteur/LandingPage/HeroSection2.tsx
+++ frontend/src/components/Recruteur/LandingPage/HeroSection2.tsx
@@ -58,7 +58,7 @@ export const HeroSectionRecruteur = ()=>{
     }
     return (
        <>
-        <div className="w-full h-screen pt-10 pb-0 bg-[url('../../src/assets/recrutePhoto.png')] bg-cover bg-no-repeat flex justify-center items-center relative ">
+        <div className="w-full h-screen pt-10  bg-[url('../../src/assets/recrutePhoto.png')] bg-cover bg-no-repeat flex justify-center items-center relative ">
         <div className="w-full flex flex-col justify-start items-end ">
             <h2 className="text-white text-5xl font-extrabold tracking-widest m-24  ">
             <span className='underline decoration-darkk'>2JOBS</span> LA PLATEFORME  <br />
@@ -74,12 +74,12 @@ export const HeroSectionRecruteur = ()=>{
             <h3 className="text-2xl font-semibold">Connectez-vous:</h3>
 
             <form
-             onSubmit={handleLoginSubmit} >
+             onSubmit={handleLoginSubmit} className='' >
                 <div className="p-3">
                     <label htmlFor="email" className="hidden mb-2 text-sm font-medium text-gray-900">Your email</label>
                     <input type="email" value={recruiterData.email}  onChange={handleInputChanges} name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 " placeholder="name@company.com" />
                 </div>
-                <div className="p-4 relative">
+                <div className="p-3 relative">
                       <label htmlFor="password" className="hidden  mb-2 text-sm font-medium text-gray-900">Password</label>
                       <input type="password" value={recruiterData.password} onChange={handleInputChanges} name="password" id="password" placeholder="••••••••" 
                       className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 "/>
@@ -102,8 +102,8 @@ export const HeroSectionRecruteur = ()=>{
                       </div>
                       <a href="#" className="text-sm font-medium text-primary-600 hover:underline ">Forgot password?</a>
                   </div>
-                  <button type="submit" className="w-full text-gray bg-fourth m-3 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-3 text-center ">Sign in</button>
-                  <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                  <button type="submit" className="w-full text-gray bg-fourth hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm  py-3 text-center ">Sign in</button>
+                  <p className="text-sm font-light pt-2 text-gray-500 dark:text-gray-400">
                       Don’t have an account yet? <a href="#" className="font-medium text-gray-500 hover:underline ">Sign up</a>
                   </p>
             </form>
