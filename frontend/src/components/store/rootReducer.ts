af3bace64e0d175a10435a52369d43a8b42ca7ca
Hatim Tagmi <125710473+theoutsider1@users.noreply.github.com>
2024-05-01 00:16:49 +0100

    Added recruiterOfferList reducer to rootReducer

--- frontend/src/components/store/rootReducer.ts
+++ frontend/src/components/store/rootReducer.ts
@@ -1,10 +1,12 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import { jobSlice } from "./features/jobDetailsSlice";
 import { whichSpaceIsActiveSlice } from "./features/espaceSlice";
+import { recruiterOffersSlice } from "./features/recruiterOffersSlice";
 
 
 export const rootReducer = combineReducers({
     getJob : jobSlice.reducer,
     space : whichSpaceIsActiveSlice.reducer,
+    recruiterOffersList : recruiterOffersSlice.reducer
 })
   
