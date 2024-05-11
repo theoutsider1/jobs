1217d073a7af1786834a77af8c20745de5b1fba9
Hatim Tagmi <125710473+theoutsider1@users.noreply.github.com>
2024-05-11 14:42:00 +0100

    Refactoring EditJobOffer.tsx

--- frontend/src/components/Recruteur/JobsManagement/EditJobOffer/EditJobOffer.tsx
+++ frontend/src/components/Recruteur/JobsManagement/EditJobOffer/EditJobOffer.tsx
@@ -103,16 +103,9 @@ export const EditJobOffer = () => {
                             htmlFor="fonction"
                             className="mb-3 block text-base font-medium text-[#07074D]"
                             >
-                            Fonction dropdown:
+                            Fonction:
                         </label>
-                        {/* <input
-                            type="text"
-                            defaultValue={field.fonction}
-                            name="fonction"
-                            id="fonction"
-                            placeholder="Fonction"
-                            className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
-                        /> */}
+                        
                         <button id="dropdownDefaultButton" onClick={()=>toggleFonctionOptions()}
                          data-dropdown-toggle="dropdown-list" className="w-full  text-white bg-fourth hover:bg-darkk font-medium rounded-lg text-sm px-5 py-3 text-center inline-flex items-center justify-between" type="button">
                             {newFonction}
