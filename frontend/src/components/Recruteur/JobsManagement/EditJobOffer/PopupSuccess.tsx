c0029431362a86c028899d3f0080024cfdc96972
Hatim Tagmi <125710473+theoutsider1@users.noreply.github.com>
2024-05-20 14:11:17 +0100

    Create PopupSuccess component

---
+++ frontend/src/components/Recruteur/JobsManagement/EditJobOffer/PopupSuccess.tsx
@@ -0,0 +1,65 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+interface SuccessPopupProps {
+    closeSuccessPopup: () => void;
+}
+
+export const PopupSuccess: React.FC<SuccessPopupProps> = ({closeSuccessPopup}) =>{
+    
+    const [updateSuccess, setUpdateSuccess] = useState(true);
+    const navigate = useNavigate();
+
+    const handleCloseSuccessPopup = () => {
+        setUpdateSuccess(false);
+        closeSuccessPopup(); // Close the popup completely
+    };
+    const redirectToDashboard = ()=> {
+        navigate(`/suivezlesoffres`)
+        window.scrollTo({
+            top: 0,
+            // behavior: 'smooth' // Optional: Smooth scrolling animation
+          });
+    }
+    return (
+        <>
+            {updateSuccess && (
+                <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+                    <div className="relative w-auto my-6 mx-auto max-w-3xl">
+                        {/*content*/}
+                        <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-third outline-none focus:outline-none">
+                            {/*header*/}
+                            {/* <div className="flex items-start justify-between p-5 border-b border-solid border-neutral-300 rounded-t">
+                                <h3 className="text-2xl font-semibold">Confirmez-vous la suppression?</h3>
+   
+                            </div> */}
+                            {/*body*/}
+                            <div className="relative p-6 flex-auto">
+                                <p className="my-4 text-blueGray-500 text-lg leading-relaxed">
+                                L'offre d'emploi a été mis à jour avec succès ! </p>
+                            </div>
+                            {/*footer*/}
+                            <div className="flex items-center justify-end p-6 border-t border-solid border-neutral-300 rounded-b">
+                                <button
+                                    className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                    type="button"
+                                    onClick={handleCloseSuccessPopup}
+                                >
+                                    Fermer
+                                </button>
+                                <button
+                                    className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                    type="button"
+                                    onClick={redirectToDashboard}
+                                >
+                                    DashBoard
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )}
+            <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+        </>
+    )
+}
