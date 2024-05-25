2d22530ff732c5b79cc912b652f5e1687c84fced
Hatim Tagmi <125710473+theoutsider1@users.noreply.github.com>
2024-05-25 17:34:25 +0100

    Fix Custom decorator GetRecruiter

--- backend/src/auth/decorator/get-recruiter.decorator.ts
+++ backend/src/auth/decorator/get-recruiter.decorator.ts
@@ -2,6 +2,7 @@ import { ExecutionContext, createParamDecorator } from "@nestjs/common";
 
 export const GetRecruiter = createParamDecorator(
     (data: unknown , ctx: ExecutionContext) => {
+  
         const request = ctx.switchToHttp().getRequest(); 
         return request.user;
     },
