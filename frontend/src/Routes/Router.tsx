2f35774055149e6836d8277ed47e3904377302a7
Hatim Tagmi <125710473+theoutsider1@users.noreply.github.com>
2024-06-07 16:01:03 +0100

    Refactor Router.tsx

--- frontend/src/Routes/Router.tsx
+++ frontend/src/Routes/Router.tsx
@@ -1,81 +1,54 @@
-import { createBrowserRouter } from "react-router-dom";
-import HeroSection from "../components/HeroSection/HeroSection";
-import  Layout  from "./Layout";
-import OffersList from "../components/Offers/OffersList";
-import JobDescription from "../components/OfferDetails/JobDescription";
-import { HeroSectionRecruteur } from "../components/Recruteur/LandingPage/HeroSection2";
-import { ContactezNous } from "../components/ContactPage/ContactezNous";
-import { AddJobComponent } from "../components/Recruteur/AddOffersPage/AddJobComponent";
-import { ChooseSpace } from "../components/ChooseSpace/ChooseSpaceT";
-import { OffersManagement } from "../components/Recruteur/JobsManagement/JobsManagement";
-import { EditJobOffer } from "../components/Recruteur/JobsManagement/EditJobOffer/EditJobOffer";
-import { LoginCanditatesForm } from "../components/Navbar/IdentificationForms/LoginForm";
-import { CreateAccountCandidates } from "../components/Navbar/IdentificationForms/CreateAccount";
+import { createBrowserRouter } from 'react-router-dom';
+import HeroSection from '../components/HeroSection/HeroSection';
+import Layout from './Layout';
+import OffersList from '../components/Offers/OffersList';
+import JobDescription from '../components/OfferDetails/JobDescription';
+import { HeroSectionRecruteur } from '../components/Recruteur/LandingPage/HeroSection2';
+import { ContactezNous } from '../components/ContactPage/ContactezNous';
+import { AddJobComponent } from '../components/Recruteur/AddOffersPage/AddJobComponent';
+import { ChooseSpace } from '../components/ChooseSpace/ChooseSpaceT';
+import { OffersManagement } from '../components/Recruteur/JobsManagement/JobsManagement';
+import { EditJobOffer } from '../components/Recruteur/JobsManagement/EditJobOffer/EditJobOffer';
+import { LoginCanditatesForm } from '../components/Navbar/IdentificationForms/LoginForm';
+import { CreateAccountCandidates } from '../components/Navbar/IdentificationForms/CreateAccount';
+import ProtectedRoute from './ProtectedRoutes';
 
 const Router = createBrowserRouter([
-    {
-        path: "/",
-        element:<Layout/>,
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      { path: '/', element: <HeroSection /> },
+      { path: '/trouverunemploi', element: <OffersList /> },
+      { path: '/description/:id', element: <JobDescription /> },
+      { path: '/EspaceRecruteur', element: <HeroSectionRecruteur /> },
+      { path: '/ContacezNous', element: <ContactezNous /> },
+      { path: '/chooseSpace', element: <ChooseSpace /> },
+      {
+        path: '/lancerUnOffre',
+        element: <ProtectedRoute rolle="recruiter" />, // Use ProtectedRoute here
         children: [
-            {
-                path: "/",
-                element: <HeroSection/>,
-            },
-            {
-                // public 
-                path: "/trouverunemploi",
-                element: <OffersList/>,
-            },
-            {
-                // public
-                path: "/description/:id",
-                element:<JobDescription/>,
-            },
-            {
-                // public and but if the user if logged in as a candidate can't access it 
-                path: "/EspaceRecruteur",
-                element:<HeroSectionRecruteur/>,
-            },
-            { 
-                // public routes
-                path: "/ContacezNous",
-                element:<ContactezNous/>,
-            },
-            {
-                // check if the user  is logged in and is a recruiter
-                path:"/lancerUnOffre",
-                element: <AddJobComponent/>,
-            },
-            {
-                // first page if the user is not logged in 
-                path:"/chooseSpace",
-                element: <ChooseSpace/>,
-            },
-            {
-                // check if the user  is logged in and is a recruiter
-                path:"/suivezlesOffres",
-                element: <OffersManagement/>,
-            },
-            {
-                // check if the user  is logged in and is a recruiter
-                path:"/suivezlesoffres/modifieroffre/:id",
-                element: <EditJobOffer/>,
-            },
-            {
-                // public
+          { path: '', element: <AddJobComponent /> }, // Nested route rendered by Outlet in ProtectedRoute
+        ],
+      },
+      {
+        path: '/suivezlesOffres',
+        element: <ProtectedRoute rolle="recruiter" />, // Use ProtectedRoute here
+        children: [
+          { path: '', element: <OffersManagement /> }, // Nested route rendered by Outlet in ProtectedRoute
+        ],
+      },
+      {
+        path: '/suivezlesoffres/modifieroffre/:id',
+        element: <ProtectedRoute rolle="recruiter" />, // Use ProtectedRoute here
+        children: [
+          { path: '', element: <EditJobOffer /> }, // Nested route rendered by Outlet in ProtectedRoute
+        ],
+      },
+      { path: '/login', element: <LoginCanditatesForm /> },
+      { path: '/signup', element: <CreateAccountCandidates /> },
+    ],
+  },
+]);
 
-                path:"/login",
-                element: <LoginCanditatesForm/>,
-            },
-            {
-                // public 
-                path:"/signup",
-                element: <CreateAccountCandidates/>,
-            },
-            
-        ]
-
-    }
-])
-
-export default Router
+export default Router;
