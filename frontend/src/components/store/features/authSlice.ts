698e4019842b4f10af8905d10b34b71289a88416
Hatim Tagmi <125710473+theoutsider1@users.noreply.github.com>
2024-06-07 15:59:46 +0100

    Edit authSlice.ts

--- frontend/src/components/store/features/authSlice.ts
+++ frontend/src/components/store/features/authSlice.ts
@@ -1,33 +1,23 @@
-import { PayloadAction, createSlice } from '@reduxjs/toolkit';
-
-type Role = 'recruiter' | 'candidate';
-
-interface LoginPayload {
-  role: Role;
-}
+import {  createSlice } from '@reduxjs/toolkit';
 
 
 const initialState = {
   isLoggedIn: false,
-  role : null as Role | null,
+  role : null as string| null,
 };
 
 const authSlice = createSlice({
   name: 'auth',
   initialState,
   reducers: {
-
-    login: (
-      state, 
-      // This Action will sent from the login form (HeroSection2 or ...)
-      action: PayloadAction<LoginPayload>) => {
-        state.isLoggedIn = true;
-        state.role= action.payload.role ;
-    },
-    logout: (state) => {
-      state.isLoggedIn = false;
-      state.role= null;
-    },
+      login: (state, action) => {
+          state.isLoggedIn = action.payload.isLogged;
+          state.role = action.payload.role;
+      },
+      logout: (state) => {
+          state.isLoggedIn = false;
+          state.role = null;
+      },
   },
 });
 
