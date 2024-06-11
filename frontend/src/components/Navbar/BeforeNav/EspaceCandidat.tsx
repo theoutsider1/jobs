7617bb3e7621f7c3d6c9ce498955993968f939a5
Hatim Tagmi <125710473+theoutsider1@users.noreply.github.com>
2024-06-11 15:46:24 +0100

    Update EspaceCandidat.tsx

--- frontend/src/components/Navbar/BeforeNav/EspaceCandidat.tsx
+++ frontend/src/components/Navbar/BeforeNav/EspaceCandidat.tsx
@@ -2,13 +2,13 @@ import { useSelector } from "react-redux";
 import { activeStateProps } from "../../../Types/Globals";
 import { RootState } from "../../store/store";
 
-export const CandidatSpace: React.FC<activeStateProps>= ({onActive})=> { 
+export const CandidatSpace: React.FC<activeStateProps>= ({onActive, roleFromAuth})=> { 
     
     const { candidatSpace } = useSelector((state: RootState) => state.space);
 
     return (
         <div>
-            <button onClick={onActive} className={`w-44 h-14 ${candidatSpace ? ' bg-darkk font-semibold' : 'bg-third'}`}>
+            <button onClick={onActive} className={`w-44 h-14 ${roleFromAuth == 'candidate' ? ' bg-darkk font-semibold' : 'bg-third'}`}>
                 Espace Candidat
             </button>
         </div>
