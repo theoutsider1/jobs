268ef7092f6c8aa5831e6321b8c877556cda7579
Hatim Tagmi <125710473+theoutsider1@users.noreply.github.com>
2024-06-11 02:36:30 +0100

    Add editJob to rootReducer

--- frontend/src/components/store/rootReducer.ts
+++ frontend/src/components/store/rootReducer.ts
@@ -3,6 +3,7 @@ import { jobSlice } from "./features/jobDetailsSlice";
 import { whichSpaceIsActiveSlice } from "./features/espaceSlice";
 import { recruiterOffersSlice } from "./features/recruiterOffersSlice";
 import authReducer from "./features/authSlice";
+import { editedJobSlice } from "./features/editJobSlice";
 
 
 export const rootReducer = combineReducers({
@@ -10,6 +11,7 @@ export const rootReducer = combineReducers({
     space : whichSpaceIsActiveSlice.reducer,
     recruiterOffersList : recruiterOffersSlice.reducer,
     auth: authReducer,
+    editJob : editedJobSlice.reducer,
     
 })
   
