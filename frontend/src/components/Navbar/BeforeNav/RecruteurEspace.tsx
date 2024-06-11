bc2a8a6d94509964011b690a38bb16b1a27cfd3e
Hatim Tagmi <125710473+theoutsider1@users.noreply.github.com>
2024-06-11 15:46:50 +0100

    Update RecruteurSpace.tsx

--- frontend/src/components/Navbar/BeforeNav/RecruteurEspace.tsx
+++ frontend/src/components/Navbar/BeforeNav/RecruteurEspace.tsx
@@ -3,12 +3,12 @@ import { useSelector } from "react-redux";
 import { activeStateProps } from "../../../Types/Globals";
 import { RootState } from "../../store/store";
 
-export const RecruterSpace: React.FC<activeStateProps>= ({onActive})=> { 
+export const RecruterSpace: React.FC<activeStateProps>= ({onActive, roleFromAuth})=> { 
     const {recruteurSpace}= useSelector((state : RootState ) => state.space)
     
     return (
         <div>
-            <button onClick={onActive}  className={`w-44 h-14 ${recruteurSpace ? ' bg-darkk font-semibold' : 'bg-third'}`}>
+            <button onClick={onActive}  className={`w-44 h-14 ${roleFromAuth == 'recruiter' ? ' bg-darkk font-semibold' : 'bg-third'}`}>
                 Espace Recruteur
             </button>
         </div>
