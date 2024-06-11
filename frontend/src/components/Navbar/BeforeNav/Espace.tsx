52d1614112717d3b1e87fa1b3436c9d494394b21
Hatim Tagmi <125710473+theoutsider1@users.noreply.github.com>
2024-06-11 15:47:07 +0100

    Update Espace.tsx

--- frontend/src/components/Navbar/BeforeNav/Espace.tsx
+++ frontend/src/components/Navbar/BeforeNav/Espace.tsx
@@ -1,13 +1,17 @@
 import { CandidatSpace } from "./EspaceCandidat";
 import { RecruterSpace } from "./RecruteurEspace";
 import { useNavigate } from "react-router-dom";
-import { useAppDispatch } from "../../store/store";
+import { RootState, useAppDispatch } from "../../store/store";
 import { candidatIsAtiveAction, recruteurIsAtiveAction } from "../../store/features/espaceSlice";
+import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+
 
 
 
 const Espace:React.FC = ()=> {
-  
+    
+    const role = useSelector((state: RootState) => state.auth.role)
     const navigate = useNavigate();
 
     {/** redux toolkit */}
@@ -16,8 +20,8 @@ const Espace:React.FC = ()=> {
     const handleRecruteurSpaceClick = ()=> {
         dispatch(recruteurIsAtiveAction())
         navigate('/EspaceRecruteur');
-        
-       
+        console.log(role);
+   
     }
     const handleCandidatSpaceClick = ()=> {
         dispatch(candidatIsAtiveAction())
@@ -25,13 +29,21 @@ const Espace:React.FC = ()=> {
         
     
     }
-   
+    const roleFromAuth = useSelector((state: RootState) => state.auth.role)
+    const [space, setSpace]= useState<string | null> (null);
+
+    useEffect(()=>{
+      if (roleFromAuth !== null && roleFromAuth == 'recruiter' ) {
+        setSpace(role)
+  
+      }
+    },[roleFromAuth])
 
     return (
         <>
             <div className="w-full h-14 bg-third flex">
-                <CandidatSpace onActive={handleCandidatSpaceClick}/>
-                <RecruterSpace onActive={handleRecruteurSpaceClick}/>
+                <CandidatSpace onActive={handleCandidatSpaceClick} roleFromAuth ={space} />
+                <RecruterSpace onActive={handleRecruteurSpaceClick} roleFromAuth ={space}/>
             </div>
         </>
     )
