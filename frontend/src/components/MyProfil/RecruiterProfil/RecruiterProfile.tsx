01463f30b3dcb13bcfcc5bfb563f07458b31c834
Hatim Tagmi <125710473+theoutsider1@users.noreply.github.com>
2024-06-19 16:03:49 +0100

    Update RecruiterProfile.tsx

--- frontend/src/components/MyProfil/RecruiterProfil/RecruiterProfile.tsx
+++ frontend/src/components/MyProfil/RecruiterProfil/RecruiterProfile.tsx
@@ -1,6 +1,10 @@
 import { IonIcon } from "@ionic/react"
 
 import { addCircleOutline } from 'ionicons/icons';
+import { personCircleOutline } from 'ionicons/icons';
+import { pencil } from 'ionicons/icons';
+import { homeOutline } from 'ionicons/icons';
+import { eyeOutline} from 'ionicons/icons';
 
 export const RecruiterProfil = ()=>{
 
@@ -8,19 +12,62 @@ export const RecruiterProfil = ()=>{
   return (
     <div className="w-full h-screen flex flex-row-4">
       {/* SideBar */}
-      <div className="w-1/4 p-8 bg-third flex flex-col items-center">
+      <div className="w-1/6 px-3 bg-third flex flex-col divide-y divide-neutral-700 items-center">
 
-        <div className="px-8 py-2 border-2 border-darkk bg-third">
-          <p className="text-xl ">Bonjour, Hatim</p>
+        <div className="w-full py-7">
+          
+            <div className="flex justify-start items-center font-semibold text-2xl gap-4">
+              <span className=""><IonIcon icon={homeOutline}  className=" cursor-pointer" /></span>
+              <h5 className="text-neutral-900 cursor-pointer">Hatim</h5>
+            </div>
+           
         </div>
-         <button>
-          <IonIcon icon={addCircleOutline}></IonIcon>
-          Ajouter un offre</button>
+         
+
+        <div className="w-full px-2 py-12  flex flex-col gap-7 justify-start items-start">
+          <button className="text-xl ">
+            
+            <div className="flex justify-center items-center opacity-65">
+            <IonIcon icon={addCircleOutline}  className="text-xl  cursor-pointer" />
+            <h5 className="px-2">Ajouter un offre</h5>
+            </div>
+            
+          </button>
+          <button className="text-xl ">
+            
+            <div className="flex justify-start items-center opacity-65">
+            <IonIcon icon={pencil}  className="text-xl cursor-pointer" />
+            <h5 className="px-2">Modifier un offre</h5>
+            </div>
+            
+          </button>
+
+          <button className="text-xl ">
+
+            <div className="flex justify-cente items-center opacity-65">
+            <IonIcon icon={eyeOutline}  className="text-xl  cursor-pointer" />
+            <h5 className="px-1">Trouver un candidat</h5>
+            </div>
+            
+          </button>
+          
+          <button className="text-xl ">
+
+            <div className="flex justify-cente items-center opacity-65">
+            <IonIcon icon={personCircleOutline}  className="text-xl  cursor-pointer" />
+            <h5 className="px-2">Mon Profile</h5>
+            </div>
+            
+          </button>
+         
+        </div>
+
+        {/* sideBar Bottom */}
          
       </div>
 
       {/* rendering components */}
-      <div className="w-3/4 flex justify-center">
+      <div className="w-5/6 flex justify-center">
         sdsadsadas
       </div>
 
