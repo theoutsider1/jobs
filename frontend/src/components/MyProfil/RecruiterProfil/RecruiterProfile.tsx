ffdf7468b4e15db4c68ee61982cd6bd0299b1764
Hatim Tagmi <125710473+theoutsider1@users.noreply.github.com>
2024-06-19 15:11:26 +0100

    Update RecruiterProfile.tsx

--- frontend/src/components/MyProfil/RecruiterProfil/RecruiterProfile.tsx
+++ frontend/src/components/MyProfil/RecruiterProfil/RecruiterProfile.tsx
@@ -1,10 +1,29 @@
+import { IonIcon } from "@ionic/react"
+
+import { addCircleOutline } from 'ionicons/icons';
+
+export const RecruiterProfil = ()=>{
 
 
+  return (
+    <div className="w-full h-screen flex flex-row-4">
+      {/* SideBar */}
+      <div className="w-1/4 p-8 bg-third flex flex-col items-center">
 
-export const RecruiterProfil = ()=> {
-    return (
-        <div className="w-full">
-
+        <div className="px-8 py-2 border-2 border-darkk bg-third">
+          <p className="text-xl ">Bonjour, Hatim</p>
         </div>
-    )
+         <button>
+          <IonIcon icon={addCircleOutline}></IonIcon>
+          Ajouter un offre</button>
+         
+      </div>
+
+      {/* rendering components */}
+      <div className="w-3/4 flex justify-center">
+        sdsadsadas
+      </div>
+
+    </div>
+  )
 }
