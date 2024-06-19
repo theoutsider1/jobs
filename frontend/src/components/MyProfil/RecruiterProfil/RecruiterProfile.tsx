feb9580db9080effe404a991297f7c283ef25bd4
Hatim Tagmi <125710473+theoutsider1@users.noreply.github.com>
2024-06-19 16:23:26 +0100

    Update RecruiterProfile SideBar

--- frontend/src/components/MyProfil/RecruiterProfil/RecruiterProfile.tsx
+++ frontend/src/components/MyProfil/RecruiterProfil/RecruiterProfile.tsx
@@ -5,6 +5,7 @@ import { personCircleOutline } from 'ionicons/icons';
 import { pencil } from 'ionicons/icons';
 import { homeOutline } from 'ionicons/icons';
 import { eyeOutline} from 'ionicons/icons';
+import { trashBinOutline} from 'ionicons/icons';
 
 export const RecruiterProfil = ()=>{
 
@@ -12,8 +13,9 @@ export const RecruiterProfil = ()=>{
   return (
     <div className="w-full h-screen flex flex-row-4">
       {/* SideBar */}
-      <div className="w-1/6 px-3 bg-third flex flex-col divide-y divide-neutral-700 items-center">
+      <div className="w-1/6 bg-third flex flex-col justify-between divide-y divide-neutral-700">
 
+      <div className="px-3 divide-y divide-neutral-700">
         <div className="w-full py-7">
           
             <div className="flex justify-start items-center font-semibold text-2xl gap-4">
@@ -44,7 +46,7 @@ export const RecruiterProfil = ()=>{
 
           <button className="text-xl ">
 
-            <div className="flex justify-cente items-center opacity-65">
+            <div className="flex justify-center items-center opacity-65">
             <IonIcon icon={eyeOutline}  className="text-xl  cursor-pointer" />
             <h5 className="px-1">Trouver un candidat</h5>
             </div>
@@ -53,16 +55,26 @@ export const RecruiterProfil = ()=>{
           
           <button className="text-xl ">
 
-            <div className="flex justify-cente items-center opacity-65">
-            <IonIcon icon={personCircleOutline}  className="text-xl  cursor-pointer" />
-            <h5 className="px-2">Mon Profile</h5>
+            <div className="flex justify-center items-center opacity-65">
+              <IonIcon icon={personCircleOutline}  className="text-xl  cursor-pointer" />
+              <h5 className="px-2">Mon Profile</h5>
             </div>
             
           </button>
          
         </div>
-
+        </div>
         {/* sideBar Bottom */}
+        <div className="mx-3 pb-12 ">
+          <div className="w-full flex justify-start items-center py-3 ">
+                <IonIcon icon={pencil} className="text-xl cursor-pointer" />
+                <h5 className="px-1 cursor-pointer " >Modifier mon mot de passe</h5>
+          </div>
+          <div className="flex justify-start items-center">
+                <IonIcon icon={trashBinOutline} className="text-xl " />
+                <h5 className="px-1 cursor-pointer ">Supprimer mon compte</h5>
+              </div>
+        </div>
          
       </div>
 
