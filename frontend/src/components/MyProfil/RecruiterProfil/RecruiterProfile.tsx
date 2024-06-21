827ca626d6948732f4aee18a2fb997b9af698ea0
Hatim Tagmi <125710473+theoutsider1@users.noreply.github.com>
2024-06-21 17:48:09 +0100

    Refactor RecruiterProfile.tsx

--- frontend/src/components/MyProfil/RecruiterProfil/RecruiterProfile.tsx
+++ frontend/src/components/MyProfil/RecruiterProfil/RecruiterProfile.tsx
@@ -7,64 +7,61 @@ import { homeOutline } from 'ionicons/icons';
 import { eyeOutline} from 'ionicons/icons';
 import { trashBinOutline} from 'ionicons/icons';
 
-import { OffersManagement } from "../../Recruteur/JobsManagement/JobsManagement";
-import { useState } from "react";
-import { AddOfferFormComponent } from "../../Recruteur/AddOffersPage/AddOfferFrom/AddOfferComponent";
+interface SidebarProps {
+  onSelect: (view: string) => void;
+  selectedView : string
+}
 
-export const RecruiterProfil = ()=>{
-  const [suiverOffre, setSuivezOffre] = useState(true);
-  const [addOffer, setAddOffer] = useState(false);
+export const RecruiterProfil: React.FC<SidebarProps> = ({onSelect, selectedView})=>{
 
   return (
-    <div className="w-full h-screen flex flex-row-4">
-      {/* SideBar */}
-      <div className="w-1/6 bg-third flex flex-col justify-between divide-y divide-neutral-700">
-
+    <div className="h-screen flex flex-col justify-between divide-y divide-neutral-700">
+      {/* sideBar Title --Home icon + name -- */}
       <div className="px-3 divide-y divide-neutral-700">
-        <div className="w-full py-7">
-          
-            <div className="flex justify-start items-center font-semibold text-2xl gap-4">
-              <span className=""><IonIcon icon={homeOutline}  className=" cursor-pointer" /></span>
-              <h5 className="text-neutral-900 cursor-pointer">Hatim</h5>
-            </div>
-           
-        </div>
+          <div className="w-full py-7">
+            
+              <div className="flex justify-start items-center font-semibold text-2xl gap-4">
+                <span className=""><IonIcon icon={homeOutline}  className=" cursor-pointer" /></span>
+                <h5 className="text-neutral-900 cursor-pointer">Hatim</h5>
+              </div>
+            
+          </div>
          
 
         <div className="w-full px-2 py-12  flex flex-col gap-7 justify-start items-start">
           {/* Mes offres */}
-          <button className="text-xl ">
-            <div className={`flex justify-start items-center${suiverOffre ? `opacity-100` : `opacity-65`}`}>
+          <button className="text-xl " onClick={() => onSelect('offersManagement')}>
+            <div className={`flex justify-start items-center ${selectedView === 'offersManagement'  ? `opacity-100` : `opacity-65`}`}>
             <IonIcon icon={pencil}  className="text-xl cursor-pointer" />
-            <h5 className={`px-2 ${suiverOffre ? `font-semibold` : ``} `}>Mes offres</h5>
+            <h5 className={`px-2 ${selectedView === 'offersManagement' ? `font-semibold` : ``} `}>Mes offres</h5>
             </div>
             
           </button>
 
             {/* Ajouter un offre */}
-          <button className="text-xl ">
+          <button className="text-xl" onClick={() => onSelect('addOffer')}>
             
-            <div className="flex justify-center items-center opacity-65">
+            <div className={`flex justify-center items-center ${selectedView === 'addOffer' ? `opacity-100` : `opacity-65`}`}>
             <IonIcon icon={addCircleOutline}  className="text-xl  cursor-pointer" />
-            <h5 className="px-2">Ajouter un offre</h5>
+            <h5 className={`px-2 ${selectedView === 'addOffer' ? `font-semibold` : ``} `}>Ajouter un offre</h5>
             </div>
             
           </button>
             {/* Trouver un candidat */}
           <button className="text-xl ">
 
-            <div className="flex justify-center items-center opacity-65">
-            <IonIcon icon={eyeOutline}  className="text-xl  cursor-pointer" />
-            <h5 className="px-1">Trouver un candidat</h5>
+            <div className={`flex justify-center items-center ${selectedView === 'findCandidat' ? `opacity-100` : `opacity-65`}`}>
+            <IonIcon icon={eyeOutline}  className="text-xl cursor-pointer" />
+            <h5 className={`px-2 ${selectedView === 'findCandidat' ? `font-semibold` : ``} `}>Trouver un candidat</h5>
             </div>
             
           </button>
             {/* Mon profile */}
-          <button className="text-xl ">
+          <button className="text-xl" onClick={() => onSelect('monProfile')}>
 
-            <div className="flex justify-center items-center opacity-65">
+            <div className={`flex justify-center items-center ${selectedView === 'monProfile' ? `opacity-100` : `opacity-65`}`}>
               <IonIcon icon={personCircleOutline}  className="text-xl  cursor-pointer" />
-              <h5 className="px-2">Mon Profile</h5>
+              <h5 className={`px-2 ${selectedView === 'monProfile' ? `font-semibold` : ``} `}>Mon Profile</h5>
             </div>
             
           </button>
@@ -85,17 +82,6 @@ export const RecruiterProfil = ()=>{
                 <h5 className="px-1 cursor-pointer ">Supprimer mon compte</h5>
               </div>
         </div>
-         
-      </div>
-
-      {/* rendering components */}
-      <div className="w-5/6 flex justify-center">
-        {suiverOffre &&  <OffersManagement/>}
-        
-        {addOffer && <AddOfferFormComponent/> }
-       
-      </div>
-
     </div>
   )
 }
