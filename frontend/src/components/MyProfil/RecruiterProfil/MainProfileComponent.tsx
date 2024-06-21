ded2a446476556fdc9035c4e5619aef24f367b44
Hatim Tagmi <125710473+theoutsider1@users.noreply.github.com>
2024-06-21 17:48:56 +0100

    Create MainProfileComponent.tsx

---
+++ frontend/src/components/MyProfil/RecruiterProfil/MainProfileComponent.tsx
@@ -0,0 +1,28 @@
+import { RecruiterProfil } from "./RecruiterProfile"
+import { OffersManagement } from "../../Recruteur/JobsManagement/JobsManagement";
+import { useState } from "react";
+import { AddOfferFormComponent } from "../../Recruteur/AddOffersPage/AddOfferFrom/AddOfferComponent";
+
+export const MainProfileComponent = ()=>{
+    const [selectedView, setSelectedView] = useState('offersManagement');
+
+  const handleSelect = (view: string) => {
+    setSelectedView(view);
+  };
+    return (
+        <div className="main-container w-full flex flex-col-6">
+            {/* SideBar */}
+            <div className="w-1/6 bg-third ">
+                <RecruiterProfil onSelect = {handleSelect} selectedView={selectedView}/>
+            </div>
+
+            {/* Selected Component */}
+            <div className="content w-5/6 flex justify-center">
+            {selectedView === 'offersManagement' &&  <OffersManagement/>}
+            {selectedView === 'addOffer' &&  <AddOfferFormComponent/>}
+            {selectedView === 'monProfile' &&  <OffersManagement/>}
+            </div>
+
+        </div>
+    )
+}
