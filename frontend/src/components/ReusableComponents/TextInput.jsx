253dc9c1b5540f40e5dbf452907b1b1c2a307857
Hatim Tagmi <125710473+theoutsider1@users.noreply.github.com>
2024-07-09 16:39:05 +0100

    Create TextInput Component

---
+++ frontend/src/components/ReusableComponents/TextInput.jsx
@@ -0,0 +1,20 @@
+import { forwardRef, useEffect, useRef } from 'react';
+
+export default forwardRef(function TextInput({ type = 'text', className = '', handleChangeForm, value, inputKey ,isFocused = false, ...props }, ref) {
+    const input = ref ? ref : useRef();
+
+    
+    return (
+        <input
+            {...props}
+            type={type}
+            className={
+                'border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm ' +
+                className
+            }
+            ref={input}
+            onChange={handleInputChange}
+            required
+        />
+    );
+});
