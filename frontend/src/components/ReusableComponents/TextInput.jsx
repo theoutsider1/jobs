c6b3a546e5a5a380050d034444512186c4913b16
Hatim Tagmi <125710473+theoutsider1@users.noreply.github.com>
2024-07-09 16:39:27 +0100

    Update TextInput.tsx

--- frontend/src/components/ReusableComponents/TextInput.jsx
+++ frontend/src/components/ReusableComponents/TextInput.jsx
@@ -2,7 +2,15 @@ import { forwardRef, useEffect, useRef } from 'react';
 
 export default forwardRef(function TextInput({ type = 'text', className = '', handleChangeForm, value, inputKey ,isFocused = false, ...props }, ref) {
     const input = ref ? ref : useRef();
-
+    useEffect(() => {
+        if (input.current && value) {
+            input.current.value = value;
+          }
+    }, [value]);
+    const handleInputChange = (e) => {
+        const { value } = e.target;
+        handleChangeForm(inputKey, value);
+    };
     
     return (
         <input
