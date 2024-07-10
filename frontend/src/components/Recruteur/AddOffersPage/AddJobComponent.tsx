0183af3cf19c09c59b559d3cb2aec5231053104c
Hatim Tagmi <125710473+theoutsider1@users.noreply.github.com>
2024-07-10 16:49:04 +0100

    Update AddJobComponent

--- frontend/src/components/Recruteur/AddOffersPage/AddJobComponent.tsx
+++ frontend/src/components/Recruteur/AddOffersPage/AddJobComponent.tsx
@@ -1,11 +1,19 @@
+
 import { AddOfferFormComponent } from "./AddOfferFrom/AddOfferComponent"
+import { RootState } from "../../store/store"
+import { useSelector } from "react-redux"
 
 
 
 export const AddJobComponent = ()=> {
 
+    // const logged = useSelector((state : RootState)=> state.auth.isLoggedIn)
+     const role = useSelector((state : RootState)=> state.auth.role)
+     const isLoggedIn = useSelector((state : RootState)=> state.auth.isLoggedIn)
+    console.log(role);
+    console.log(isLoggedIn);
 
-
+    
     return (
         <div className="w-full">
 
