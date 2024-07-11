4961e8abcc7a09968c74fedd732bc025bb5d52a0
Hatim Tagmi <125710473+theoutsider1@users.noreply.github.com>
2024-07-11 15:21:07 +0100

    Create RecruiterProfileInfo

---
+++ frontend/src/components/Recruteur/RecruiterInfos/RecruiterProfileInfo.tsx
@@ -0,0 +1,49 @@
+export const RecruiterProfileInfo = ()=>{
+
+    return(
+        <div className="w-full">
+            <div className="p-12 font-semibold text-2xl text-center">
+                <h3>Mon Profile</h3>
+            </div>
+            <div className="flex m-8 px-12">
+                <div className="w-2/6 flex ">
+                     <h3>Mon Profile</h3>
+                </div>
+
+                <div className="w-4/6">
+
+                    <div className="py-2">
+                        <label htmlFor="nomComplet" className="w-full text-xl font-semibold">
+                            <span>Nom complet</span>
+                        </label>
+                            <input type="text" id="nomComplet" className="my-4 block w-full rounded-md border-0 py-1.5 pl-3 pr-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 " />
+                  
+                    </div>
+                    <div className="py-2">
+                        <label htmlFor="nomComplet" className="w-full text-xl font-semibold">
+                            <span className="">Nom complet</span>
+                        </label>
+                            <input type="text" id="nomComplet" className=" my-4 block w-full rounded-md border-0 py-1.5 pl-3 pr-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 " />
+                  
+                   </div>
+                   <div className="py-2">
+                        <label htmlFor="nomComplet" className="w-full text-xl font-semibold">
+                            <span>Nom complet</span>
+                        </label>
+                            <input type="text" id="nomComplet" className="my-4 block w-full rounded-md border-0 py-1.5 pl-3 pr-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 " />
+                  
+                   </div>
+                   <div className="py-2">
+                        <label htmlFor="nomComplet" className="w-full text-xl font-semibold">
+                            <span>Nom complet</span>
+                        </label>
+                            <input type="text" id="nomComplet" className="my-4 block w-full rounded-md border-0 py-1.5 pl-3 pr-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 " />
+                  
+                   </div>
+                </div>
+
+            </div>
+            
+        </div>
+    )
+}
