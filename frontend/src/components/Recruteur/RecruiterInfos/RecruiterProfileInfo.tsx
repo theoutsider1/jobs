a7865887b49966f1df5461feea5c98caf4df85bd
Hatim Tagmi <125710473+theoutsider1@users.noreply.github.com>
2024-07-12 16:13:44 +0100

    Update RecruiterProfileInfo component

--- frontend/src/components/Recruteur/RecruiterInfos/RecruiterProfileInfo.tsx
+++ frontend/src/components/Recruteur/RecruiterInfos/RecruiterProfileInfo.tsx
@@ -7,39 +7,33 @@ export const RecruiterProfileInfo = ()=>{
             </div>
             <div className="flex m-8 px-12">
                 <div className="w-2/6 flex ">
-                     <h3>Mon Profile</h3>
+                     <h3>Photo</h3>
                 </div>
 
                 <div className="w-4/6">
 
                     <div className="py-2">
-                        <label htmlFor="nomComplet" className="w-full text-xl font-semibold">
+                        <label htmlFor="fullName" className="w-full text-xl font-semibold">
                             <span>Nom complet</span>
                         </label>
-                            <input type="text" id="nomComplet" className="my-4 block w-full rounded-md border-0 py-1.5 pl-3 pr-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 " />
+                            <input type="text" id="fullName" className="my-4 block w-full rounded-md border-0 py-1.5 pl-3 pr-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 " />
                   
                     </div>
                     <div className="py-2">
-                        <label htmlFor="nomComplet" className="w-full text-xl font-semibold">
-                            <span className="">Nom complet</span>
+                        <label htmlFor="mail" className="w-full text-xl font-semibold">
+                            <span className="">mail</span>
                         </label>
-                            <input type="text" id="nomComplet" className=" my-4 block w-full rounded-md border-0 py-1.5 pl-3 pr-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 " />
+                            <input type="text" id="mail" className=" my-4 block w-full rounded-md border-0 py-1.5 pl-3 pr-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 " />
                   
                    </div>
                    <div className="py-2">
-                        <label htmlFor="nomComplet" className="w-full text-xl font-semibold">
-                            <span>Nom complet</span>
+                        <label htmlFor="phone_number" className="w-full text-xl font-semibold">
+                            <span>Numero de Telephone</span>
                         </label>
-                            <input type="text" id="nomComplet" className="my-4 block w-full rounded-md border-0 py-1.5 pl-3 pr-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 " />
+                            <input type="text" id="phone_number" className="my-4 block w-full rounded-md border-0 py-1.5 pl-3 pr-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 " />
                   
                    </div>
-                   <div className="py-2">
-                        <label htmlFor="nomComplet" className="w-full text-xl font-semibold">
-                            <span>Nom complet</span>
-                        </label>
-                            <input type="text" id="nomComplet" className="my-4 block w-full rounded-md border-0 py-1.5 pl-3 pr-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 " />
                   
-                   </div>
                 </div>
 
             </div>
