ad6afb4aa45c222247f16f5cabeb41c0bac0aca7
Hatim Tagmi <125710473+theoutsider1@users.noreply.github.com>
2024-08-04 14:58:33 +0100

    Update search input

--- frontend/src/components/Recruteur/JobsManagement/JobsManagement.tsx
+++ frontend/src/components/Recruteur/JobsManagement/JobsManagement.tsx
@@ -263,7 +263,14 @@ export const OffersManagement = () =>{
                                         <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
                                     </svg>
                                 </div> 
-                                <input type="search" id="default-search" className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-third focus:border-third" placeholder="Recherche" required />
+                                <input 
+                                    type="search" 
+                                    value={searchQuery}
+                                    onChange={handleSearchQuery}
+                                    id="default-search" 
+                                    className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-third focus:border-third" 
+                                    placeholder="Recherche" 
+                                    required />
                             </div>
                         
                     </div>
