a0664004e4392092db81060662cdf7da121727fa
Hatim Tagmi <125710473+theoutsider1@users.noreply.github.com>
2024-08-04 14:53:48 +0100

    Add searchQuery state & Update input search

--- frontend/src/components/Recruteur/JobsManagement/JobsManagement.tsx
+++ frontend/src/components/Recruteur/JobsManagement/JobsManagement.tsx
@@ -23,11 +23,10 @@ export const OffersManagement = () =>{
     const [inprogressOffers, setInprogressOffers] = useState(false);
     const [closedOffers , setClosedOffers] = useState(false);
     const [allData , setAllData ] = useState<OffData []>([]);
-    
+    const [searchQuery, setSearchQuery] = useState(String)
 
     const [allOffersLength ,setAllOffersLength] = useState<number>(0);
 
-    // IMPORTANT : WILL be calculated later once the schema has been modified
     
     const [inprogressOffersLength ,setInprogressOffersLength] = useState<number>(0);
     const [closedOffersLength ,setClosedOffersLength] = useState<number>(0);
@@ -255,8 +254,8 @@ export const OffersManagement = () =>{
                         id="searchJob"
                         className="  w-1/2 rounded-md border-0 py-2 m-2 text-gray-900 ring-1 ring-inset ring-gray-300 "/>
                      */}
-                     <div className="w-1/3">
-                        <form className="py-4">   
+                     <div className="w-1/3 py-4">
+                           
                             <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
                             <div className="relative">
                                 <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
@@ -265,9 +264,8 @@ export const OffersManagement = () =>{
                                     </svg>
                                 </div> 
                                 <input type="search" id="default-search" className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-third focus:border-third" placeholder="Recherche" required />
-                                <button type="submit" className="text-white absolute end-2.5 bottom-2.5 bg-secondary  hover:bg-primary focus:ring-4 focus:ring-third focus:outline-none  font-medium rounded-lg text-sm px-4 py-2 ">Search</button>
                             </div>
-                        </form>
+                        
                     </div>
                     
                 </div>
